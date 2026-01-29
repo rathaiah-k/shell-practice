ba2fae60b52a00f07f46684b8be576ccc70e01e0
Rathaiah <kamma.rathaiah@gmail.com>
2026-01-29 22:06:59 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PERSON1=Krisna #no space between value and equal
-PERSON2=Partha
+PERSON1=Trumph #no space between value and equal
+PERSON2=Putin
 echo "$PERSON1::Hi Partha , how are you"
 echo "$PERSON2:: Hi Krishna , I am doing good"
