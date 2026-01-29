88366abddcf21b0d7b6d8d68c277c04e9bc4a53f
Rathaiah <kamma.rathaiah@gmail.com>
2026-01-29 22:18:43 +0530

    actions

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PERSON1=$1 #no space between value and equal
 PERSON2=$2
-echo "$PERSON1::Hi Partha , how are you"
-echo "$PERSON2:: Hi Krishna , I am doing good"
+echo "$PERSON1::Hi $PERSON2 , how are you"
+echo "$PERSON2:: Hi $PERSON1 , I am doing good"
